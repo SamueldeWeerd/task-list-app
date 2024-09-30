40dd6c5639fdea2d9f87a69d7e46bd386b428d7b
Samuel <samueldeweerd@gmail.com>
2024-09-30 11:59:25 +0200

    Set up a custom taskOptionsProvider in order to clean up the DOM code. We can then loop for different options and use the values from the provider.

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,4 +1,37 @@
+import { InjectionToken, Provider } from "@angular/core";
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
+
+type TaskStatusOptions = {
+  value: 'open' | 'in-progress' | 'done',
+  taskStatus: TaskStatus,
+  text: string
+}[] 
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>('task-status-options');
+
+export const taskStatusOptions: TaskStatusOptions= [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open'
+  }, 
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'in progress'
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Done'
+  }
+]
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: taskStatusOptions
+};
 
 export interface Task {
   id: string;
--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskService } from '../task.service';
+import { TASK_STATUS_OPTIONS, taskStatusOptions, taskStatusOptionsProvider} from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,9 +10,13 @@ import { TaskService } from '../task.service';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [
+    taskStatusOptionsProvider
+  ]
 })
 export class TasksListComponent {
   private taskService = inject(TaskService);
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
   selectedFilter = signal<string>('all');
   tasks = computed(() => {
     switch (this.selectedFilter()) {
